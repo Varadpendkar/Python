448ea1a858ac8c6347188fd7888be122a421fb4a
Varadpendkar <123836771+Varadpendkar@users.noreply.github.com>
2025-09-16 00:49:06 +0530

    Add files via upload

---
+++ 27th_task2/variable.py
@@ -0,0 +1,38 @@
+# Variable in python 
+first_name = "Varad"
+last_name = "Pendkar"
+Country = "India"
+City = "Pune"
+Age = 20
+is_married = False
+SKILLS = ["Python", "Java", "C++"]
+person_info = {
+  'first_name':'Asabeneh',
+  'last_name':'Yetayeh',
+  'country':'Finland',
+  'city':'Helsinki',
+}
+
+# printing the values stored in the variables 
+print('First name:',first_name)
+print('First name length:',len(first_name))
+print('Last name:',last_name)
+print('Last name lenght:',len(last_name))
+print('Country:',Country)
+print('City:',City)
+print('Age:',Age)
+print('Is married:',is_married)
+print('Skills:',SKILLS)
+print('Person info :',person_info)
+
+
+# Declaring mulriple variables in one line 
+first_name, last_name, Country, City,Age, is_married = "Varad", "Pendkar", "India", "Pune", 20, False 
+
+print(first_name,last_name,Country,City,Age,is_married)
+print('First name:',first_name)
+print('Last name :',last_name)
+print('Country:',Country)
+print('City :',City)
+print('Aqe:',Age)
+print('Is married:',is_married)
